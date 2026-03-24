74ad7a57d144137847250f3ee25e9a08c28b09ea
paige <paigely@tuta.io>
2026-03-24 20:14:37 +0100

    chore: bump versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -179,8 +179,8 @@ android {
 		applicationId = "paige.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 18
-		versionName = "v1.0.0-alpha28"
+		versionCode = 19
+		versionName = "v1.0.0-alpha29"
 	}
 
 	signingConfigs {
